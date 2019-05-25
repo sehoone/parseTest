622ca1f704dd76a1d1273d8e3e75a91d1c9a4bd1
세훈이 <@DESKTOP-EJEMAMM>
2019-05-25 21:30:52 +0900

    tag tree 추가

--- js/custom.js
+++ js/custom.js
@@ -76,14 +76,11 @@ $('#tagTable').DataTable({
 
 
 var vectorChartData = resultJson['# 이벤트 위치 #']['# 이벤트 위치 # 국가 ID,이벤트 수'];
-console.log('vectorChartData' + JSON.stringify(vectorChartData, null, 2));
 var vectorChartJsObj = {};
 for (var i = 0; i < vectorChartData.length; i++) {
 	var cdataVal = vectorChartData[i];
-	console.log('cdataVal' + JSON.stringify(cdataVal, null, 2));
 	vectorChartJsObj[cdataVal['국가 ID'].toLowerCase()] = cdataVal['이벤트 수'] 
 }
-console.log('vectorChartJsObj' + JSON.stringify(vectorChartJsObj, null, 2));
 $('#world-map-gdp').vectorMap({
 	
 	map: 'world_en',
@@ -111,4 +108,158 @@ $('#world-map-gdp').vectorMap({
         onRegionTipShow: function(e, el, code){
           el.html(el.html()+' (GDP - '+vectorChartJsObj[code]+')');
         }*/
+});
+
+var tagTreeData = resultJson['# tag #']['# tag # 맞춤 매개변수,이벤트 수,사용자'];
+
+var tagTreeShuitArr = [];
+for (var i = 0; i < tagTreeData.length; i++) {
+	tagTreeShuitArr.push( tagTreeData[i]['맞춤 매개변수'] );
+}
+var tagTreeJson = []
+tagTreeShuitArr.forEach(function (item, index, array) {
+    var keys = item.split(':'),
+    last = keys.pop();
+
+keys.reduce(function (r, k) {
+    if (!r[k]) {
+        r[k] = { _: [] };
+        r._.push({ text: k, children: r[k]._ });
+    }
+    return r[k];
+}, this)._.push({ text: last });
+}, { _: tagTreeJson });
+/*
+function getAllSelectedNodesText(jsTree) {
+	   // this returns ids of all selected nodes
+	   var selectedNodes = jsTree.jstree("get_selected"); 
+
+	   var allText = [];
+
+	   // Go through all selected nodes to get text (jquery)
+	   $.each(selectedNodes, function (i, nodeId) {
+	       var node = jsTree.jstree("get_node", nodeId);
+	       allText.push(node.text); // Add text to array
+	   });
+
+	   return allText.join(); // This will join all entries with comma
+	}
+
+function getNodeById(id, node){
+    var reduce = [].reduce;
+    function runner(result, node){
+        if(result || !node) return result;
+        return node.id === id && node || //is this the proper node?
+            runner(null, node.children) || //process this nodes children
+            reduce.call(Object(node), runner, result);  //maybe this is some ArrayLike Structure
+    }
+    return runner(null, node);
+}
+
+function uiGetParents(loSelectedNode) {
+    try {
+        var loData = [];
+        var lnLevel = loSelectedNode.node.parents.length;
+        var lsSelectedID = loSelectedNode.node.id;
+        var loParent = $("#" + lsSelectedID);
+        var lsParents = loSelectedNode.node.text + ' >';
+        for (var ln = 0; ln <= lnLevel - 1 ; ln++) {
+            var loParent = loParent.parent().parent();
+            if (loParent.children()[1] != undefined) {
+                lsParents += loParent.children()[1].text + " > ";
+                loData.push(loParent.children()[1].text);
+            }
+        }
+        if (lsParents.length > 0) {
+            lsParents = lsParents.substring(0, lsParents.length - 1);
+        }
+        //alert(lsParents);
+        //alert(loData.reverse());
+    }
+    catch (err) {
+        alert('Error in uiGetParents');
+    }
+}*/
+
+$('#tagTree').jstree({
+	'core' : {
+		'data' : tagTreeJson
+	},
+    "checkbox": {
+        "keep_selected_style": false
+    },
+        "plugins": ["checkbox"]
+});
+
+
+$(document).ready(function() {
+	/*
+	$('#tagTree').on('select_node.jstree', function (e, data) {
+	    var loMainSelected = data;
+	    uiGetParents(loMainSelected);
+	});
+	
+	$('#tagTree').on('deselect_node.jstree', function (e, data) {
+	    //var loMainSelected = data;
+	    //uiGetParents(loMainSelected);
+	});*/
+	$('#allText').click(function() {
+		$('#searchTagTable').DataTable().destroy();
+		var selectTagDeptVal = []; 
+		var node=$('#tagTree').jstree("get_selected", true);
+		$.each(node, function (i, item) {
+			selectTagDeptVal.push($('#tagTree').jstree().get_path(node[i], ':'));
+		   });
+		var searchTagTableData = resultJson['# tag #']['# tag # 맞춤 매개변수,이벤트 수,사용자'];
+		var searchedTagTableData = [];
+		$.each(searchTagTableData, function (i, item) {
+			
+			var suiteVal = item['맞춤 매개변수'];
+			if(selectTagDeptVal.includes(suiteVal)){
+				
+				searchedTagTableData.push(item);
+			}
+		   });
+		
+		$('#searchTagTable').DataTable({
+			data : searchedTagTableData,
+			columns : [ {
+				"data" : "맞춤 매개변수"
+			}, {
+				"data" : "이벤트 수"
+			}, {
+				"data" : "사용자"
+			} ],
+			searching: false,
+			drawCallback: function ( row, data, start, end, display ) {
+	            var api = this.api(), data;
+	 
+	            // Remove the formatting to get integer data for summation
+	            var intVal = function ( i ) {
+	                return typeof i === 'string' ?
+	                    i.replace(/[\$,]/g, '')*1 :
+	                    typeof i === 'number' ?
+	                        i : 0;
+	            };
+	 
+	            // Total over all pages
+	            totalEvent = api
+	                .column( 1 )
+	                .data()
+	                .reduce( function (a, b) {
+	                    return intVal(a) + intVal(b);
+	                }, 0 );
+	 
+	            totalUser = api
+                .column( 2 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+ 
+	            $('#sumEvent').text('이벤트합계  : '+ totalEvent);
+	            $('#sumUser').text('사용자합계  : '+ totalUser);
+	        }
+		});
+	});
 });
